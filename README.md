8fefae55c2c61f581f9e49b52eb6c939a8584aa8
Mihail2403 <mihailpickalev@gmail.com>
2024-04-07 13:15:53 +0500

    auth_service

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# AuthGo
+# AUTH Service
+
+## start:
+
+- docker-compose up -d
+
+## stop:
+
+- docker-compose down
+
+## api-url:
+
+    http://localhost:8000/auth
---
+++ internal/transport/http/handler.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"auth/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+// handler struct
+type Handler struct {
+	service *service.Service
+}
+
+// Constructor Handler
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
+
+// Register gin router
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.SignUp)
+		auth.POST("/sign-in", h.SignIn)
+		auth.GET("/get-by-token", h.GetUserByToken)
+	}
+	return router
+}
